2bd12fdb7519ed7a269bb5caae416051f9155b3f
origin <nkrenta@inbox.ru>
2024-12-29 22:41:04 +0300

    Init

---
+++ src/test/java/com/skypro/demo/TrashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
